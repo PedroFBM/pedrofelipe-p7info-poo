16f8f0e93e7d15a74c342e05ed1347edd7388a37
PedroFBM <99341598+PedroFBM@users.noreply.github.com>
2022-05-21 23:37:00 -0300

    reformulação imprimir nota fiscal

--- atividade-avaliação/Avaliação 4 - 5/nota_fiscal.ts
+++ atividade-avaliação/Avaliação 4 - 5/nota_fiscal.ts
@@ -67,15 +67,27 @@ export class NotaFiscal {
 
     // Imprimir a NotaFiscal conforme o Layout definido
     
-    notaFiscal(): void {
-        let total = 0
-        for(let count = 0; count < this.items.length; count++){
-            let quantia =  this.items[count].getquantidade() * this.items[count].getvalor()
-            total += quantia
+    imprimirNotaFiscal(): void {
+        var data = new Date();
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("NOTA FISCAL                                                                                                   ", data)
+        console.log("Cliente:",this.cliente.getcodigo(), "      Nome:",this.cliente.getnome() )
+        console.log("CPF:",this.cliente.getcnpjcpf())
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("ITENS")
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("Seq    Descrição                                                               QTD       Valor Unit         Preço   ")
+        console.log("------------------------------------------------------------------------      -----     -------------     ----------")
+        for(let a = 0; a < this.items.length; a++){
+            let precoTotal =  this.items[a].getquantidade() * this.items[a].produto.getvalorUnitario()
+            this.valorNota += precoTotal
+            console.log("%i       %s                                                       %f            %f              %f  ",
+            this.items[a].getsequencial(), this.items[a].produto.getdescricao(), this.items[a].getquantidade(), this.items[a].produto.getvalorUnitario(), precoTotal)
         }
-
-        console.log("Valor total: R$ " + total)
+        console.log("--------------------------------------------------------------------------------------------------------------------")
+        console.log("Valor Total:", this.valorNota)
     }
+
 
     
 }
